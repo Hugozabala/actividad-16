1716f1cc9e9122ac6cc578a93c0e86bda8487d39
Usuario <hugocanizzabala@gmail.com>
2025-08-13 23:04:59 -0600

    clace 3

--- actividad 16.py
+++ actividad 16.py
@@ -1,17 +1,36 @@
+
 class biblioteca:
-    def __init__(self,titulo,autor,año,codigo ):
+    def __init__(self,codigo,titulo,autor,año ):
+        self.codigo = codigo
         self.titulo = titulo
         self.autor= autor
         self.año = año
-        self.codigo=codigo
+
 
     def mostrar_info(self):
-        return f"Titulo: {self.titulo} - autor: {self.autor} - año: {self.año} - codigo: {self.codigo}"
+        return f"codigo: {self.codigo}-Titulo: {self.titulo} - autor: {self.autor} - año: {self.año} "
 
 
 class Registro_libro:
     def __init__(self):
         self.libro = {}
+
+
+    def registro_libro(self):
+        try:
+            codigo=input("ingrese codigo de libro")
+            if codigo in self.libro:
+                print("ya existe un libro con ese codigo")
+                return
+            titulo=input("ingrese titulo ")
+            autor= input("ingrese autor de libro")
+            año=int(input("ingrse año de libro ejp. 1993"))
+
+            self.libro[codigo]=biblioteca(codigo,titulo,autor,año)
+            print("libro agregado con exito. \n")
+
+        except ValueError:
+            print("dato incorrecto")
 
 
 class estudiante:
@@ -66,15 +85,19 @@ def menu():
     print("=======menu principal======")
     print("1. regitro de libros ")
     print("2. registro de estudiante")
-    print("3. buscar libro")
-    print("4. buscar esudiante ")
-    print("5. salir")
+    print("3. prestar libro")
+    print("4. devolver libro ")
+    print("5. eliminar libro")
+    print("6. agregar libro ")
+    print("7. eliminar estudiante ")
+    print("8. agregar estudiante")
+    print("9. salir")
 
 
 registro = RegistroEstudiantes()
 op=0
 
-while op!=5:
+while op!=9:
     menu()
     try:
         op=int(input("ingrese una opcion a ejecutar"))
@@ -82,12 +105,20 @@ while op!=5:
             case 1:
                 pass
             case 2:
-
+                pass
             case 3:
                 pass
             case 4:
                 pass
             case 5:
+                pass
+            case 6:
+                pass
+            case 7:
+                pass
+            case 8:
+                pass
+            case 9:
                 print("fin del progrma")
             case _:
                 print("has ingresado una opcion invalida")
